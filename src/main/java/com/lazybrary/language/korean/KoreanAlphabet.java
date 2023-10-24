44d1ca46adf085caee59e17c719166445725e9d8
songsirr <fkshwy@naver.com>
2023-10-24 10:07:16 +0900

    [language util] - korean util

---
+++ src/main/java/com/lazybrary/language/korean/KoreanAlphabet.java
@@ -0,0 +1,15 @@
+package com.lazybrary.language.korean;
+
+public class KoreanAlphabet {
+
+    public final static char KOREAN_UNI = 0xAC00;
+
+    // Ja
+    public final static String[] VAL1 = {"ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"};
+
+    // Mo
+    public final static String[] VAL2 = {"ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"};
+
+    // Ja, but it must be placed at the end
+    public final static String[] VAL3 = {"", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"};
+}
