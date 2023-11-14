8acd2617867b6f088886ce8b2492f0ff23fe1adc
songsirr <fkshwy@naver.com>
2023-11-14 17:05:30 +0900

    [web util] - random password

---
+++ src/main/java/com/lazybrary/web/WebUtil.java
@@ -0,0 +1,39 @@
+package com.lazybrary.web;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class WebUtil {
+
+    public static String generateRandomPassword(int length){
+
+        char[] SYMBOLS = "!@#&()–[{}]:;',?/*~$^+=<>".toCharArray();
+        char[] LOWERCASE = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        char[] NUMBERS = "0123456789".toCharArray();
+        char[] ALL_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789^$*.[]{}()?-\"!@#%&/\\,><':;|_~`".toCharArray();
+        Random rand = new SecureRandom();
+        char[] password = new char[length];
+
+        //get the requirements out of the way
+        password[0] = LOWERCASE[rand.nextInt(LOWERCASE.length)];
+        password[1] = UPPERCASE[rand.nextInt(UPPERCASE.length)];
+        password[2] = NUMBERS[rand.nextInt(NUMBERS.length)];
+        password[3] = SYMBOLS[rand.nextInt(SYMBOLS.length)];
+
+        //populate rest of the password with random chars
+        for (int i = 4; i < length; i++) {
+            password[i] = ALL_CHARS[rand.nextInt(ALL_CHARS.length)];
+        }
+
+        //shuffle it up
+        for (int i = 0; i < password.length; i++) {
+            int randomPosition = rand.nextInt(password.length);
+            char temp = password[i];
+            password[i] = password[randomPosition];
+            password[randomPosition] = temp;
+        }
+
+        return new String(password);
+    }
+}
