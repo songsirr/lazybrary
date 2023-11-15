07cea63ef82a2a1ad9d5feb98e95f932319fcce2
songsirr <fkshwy@naver.com>
2023-11-15 11:35:22 +0900

    [object util] - refactoring

--- src/main/java/com/lazybrary/object/ObjectUtil.java
+++ src/main/java/com/lazybrary/object/ObjectUtil.java
@@ -1,22 +1,21 @@
 package com.lazybrary.object;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class ObjectUtil {
 
     public static boolean hasNullField(Object o) {
-        for (Field f : o.getClass().getDeclaredFields()){
-            f.setAccessible(true);
-            try {
-                if (f.get(o) == null){
-                    return true;
-                }
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return false;
+        return Arrays.stream(o.getClass().getDeclaredFields())
+                .peek(field -> field.setAccessible(true))
+                .anyMatch(field -> {
+                    try {
+                        return field.get(o) == null;
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
     public static boolean isNullObject(Object o){
@@ -39,7 +38,8 @@ public class ObjectUtil {
             f.setAccessible(true);
             try {
                 if (f.getType().equals(String.class)){
-                    if (f.get(o).equals("")){
+                    Object value = f.get(o);
+                    if (value != null && value.equals("")) {
                         f.set(o, null);
                     }
                 }
