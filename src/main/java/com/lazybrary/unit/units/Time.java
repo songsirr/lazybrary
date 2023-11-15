aca6b71fa636740af57d506f601eaa58c413cee9
songsirr <fkshwy@naver.com>
2023-11-15 11:09:12 +0900

    [unit converter] - time converter

---
+++ src/main/java/com/lazybrary/unit/units/Time.java
@@ -0,0 +1,42 @@
+package com.lazybrary.unit.units;
+
+import com.lazybrary.unit.exception.NotSupportedTypeException;
+
+public enum Time {
+
+    NANOSECOND("nanosecond", "ns", 1),
+    MICROSECOND("microsecond", "μs", NANOSECOND.value * 1000),
+    MILLISECOND("millisecond", "ms", MICROSECOND.value * 1000),
+    SECOND("second", "s", MILLISECOND.value * 1000),
+    MINUTE("minute", "m", SECOND.value * 60),
+    HOUR("hour", "h", MINUTE.value * 60),
+    DAY("day", "d", HOUR.value * 24),
+    WEEK("week", "w", DAY.value * 7);
+
+    private final String lowerCase;
+
+    private final String symbol;
+
+    private final double value;
+
+    Time(String lowerCase, String symbol, double value) {
+        this.lowerCase = lowerCase;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    public static Time of(String unit){
+        // try to find by Unit name or Symbol
+        for (Time t : values()){
+            if (t.lowerCase.equalsIgnoreCase(unit) || t.symbol.equalsIgnoreCase(unit)){
+                return t;
+            }
+        }
+
+        throw new NotSupportedTypeException(Length.class, unit);
+    }
+
+    public double convertTo(Time toUnit, double value) {
+        return value * this.value / toUnit.value;
+    }
+}
